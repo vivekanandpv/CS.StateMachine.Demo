9cf21396ad024a9c953d7211d63554b9cb0ae726
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-16 10:51:53 +0530

    done

--- Program.cs
+++ Program.cs
@@ -5,29 +5,72 @@ namespace CS.StateMachine.Demo
 {
     internal class Program
     {
+        static StateMachine sm = new StateMachine();
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(() =>
+            sm.Start(14);
+
+            sm.OnDone += (sender, eventArgs) =>
             {
-                PrintMessage("XYZ");
-            }) {Name = "Thread 1"};
+                try
+                {
+                    Console.WriteLine($"Work is done, the result is {sm.Result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Catching the exception in Main! {e.Message}");
+                }
+               
+            };
 
-            Thread t2 = new Thread(() =>
-            {
-                PrintMessage("ABC");
-            }) {Name = "Thread 2"};
-
-            t1.Start();
-            t2.Start();
-
-            t1.Join();
-            t2.Join();
+            Console.WriteLine("Main ends!");
         }
+    }
 
-        static void PrintMessage(string message)
+
+    class StateMachine
+    {
+        private int _result = default(int);
+        private Exception _exception = default(Exception);
+
+        public bool IsFinished { get; set; }
+        
+        public bool IsFaulted { get; set; }
+        public Exception Error => _exception;
+
+        public event EventHandler OnDone; 
+
+        public int Result => _exception != null ? throw _exception : _result;
+
+
+
+        public void Start(int n)
         {
-            string lv = "Good morning";
-            Console.WriteLine($"Thread: {Thread.CurrentThread.Name} says: {lv}; message is: {message}");
+
+            Thread t = new Thread(() =>
+            {
+                try
+                {
+                    Thread.Sleep(1200);
+                    
+                    _result = n * n;
+
+                    if (_result == 196)
+                    {
+                        throw new Exception("Purposely!");
+                    }
+                    IsFinished = true;
+                    OnDone?.Invoke(this, null);
+                }
+                catch (Exception e)
+                {
+                    IsFaulted = true;
+                    _exception = e;
+                    OnDone?.Invoke(this, null);
+                }
+            });
+
+            t.Start();
         }
     }
 
